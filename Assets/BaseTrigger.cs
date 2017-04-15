a98b0b13e451a95334ef73d3fc32c6497d97af61
yanagiragi <qaz90473@gmail.com>
2017-04-16 00:37:45 +0800

    Init

---
+++ Assets/BaseTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ *  Author:     yanagiragi 
+ *  Purpose:    Base Class of Trigger Function
+ */
+
+public class BaseTrigger : MonoBehaviour
+{
+    public delegate void exceutable();
+    protected exceutable p_Instance = null; // should be initialized in child class
+
+    public exceutable getTrigger()
+    {
+        if (p_Instance == null)
+        {
+            // throw Exception is Better?
+            Debug.LogError("Attempt to call trigger function before initialized it.");
+            return delegate { };
+        }
+        else
+        {
+            return p_Instance;
+        }        
+    }
+}
---
+++ Assets/NierTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ *  Author:     yanagiragi 
+ *  Purpose:    用來測試事件的Script，綁在測試模型(Yorha 2B)上
+ */
+
+public class NierTrigger : BaseTrigger {
+
+    // 因為要讓外面呼叫所以要用public
+    public void publicTrigger()
+    {
+        base.getTrigger()();
+    }
+
+    // Contructor
+    public NierTrigger() : base()
+    {
+        // 如果直接用 base 的 contructor 呼叫，會因為現在這個物件還沒建立無法存取到 this.trigger() 的Instance.
+        // 因此初始化 p_Instance 在 child Class 內進行。
+        base.p_Instance = new BaseTrigger.exceutable(trigger);
+    }
+
+
+    // ============================ 以下自定義 ============================ //
+
+    // 自定義 function callback
+    protected void trigger() 
+    {
+        Debug.Log("Gaze At 2B");
+    }
+
+    void Start() { }
+
+    void Update() { }
+}
